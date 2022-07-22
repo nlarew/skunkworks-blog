5c972d565325174749cc536a8b1f43dc21e6b7dc
Nick Larew <nick.larew@mongodb.com>
2022-07-22 13:27:11 -0400

    no search

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Search from "atlas-static-site-search-box"
+// import Search from "atlas-static-site-search-box"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -26,7 +26,7 @@ const Layout = ({ location, title, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">
         {header}
-        <Search id="docs-search-wpvpm" />
+        {/* <Search id="docs-search-wpvpm" /> */}
       </header>
       <main>{children}</main>
       <footer>
